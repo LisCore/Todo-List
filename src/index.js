430933f8e834ce96250f1006258b454a271f5577
Corey Lee <clee66909@gmail.com>
2024-08-04 14:58:25 -0700

    Added majority of dependencies for webpack, linked them in config and added needed directories

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log("123");
+document.addEventListener("DOMContentLoaded", function() {
+    const body = document.querySelector("body");
+    let textMessage = document.createElement("h1");
+    textMessage.textContent = "Hello World!";
+    body.appendChild(textMessage);
+});
+
